755c14f55ec8e95f2217e928c5f0b536aedfa4db
edoran <emd218@lehigh.edu>
2014-08-27 16:48:42 +0000

    Homework submission

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+//////////////////////////////////////////////////////////////////////////////
+//Eric Doran
+//Welcome Class Java Program
+//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+        
+//  print the statement 
+    System.out.println("  -------------     ");
+    System.out.println("     WELCOME        ");
+    System.out.println("  -------------     ");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println(" -E--M--D--2--1--8- ");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println("  v  v  v  v  v  v  ");
+    System.out.println("I am an Eagle Scout, and am an aspiring engineer");
+    
+    }
+}
